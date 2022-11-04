866550341c911a22b3e850245cf787a0fc880dd3
Yang Xu <yangxu011900@gmail.com>
2022-11-03 21:08:44 -0400

    added saving data to file

--- output.c
+++ output.c
@@ -1,26 +1,26 @@
 
 /*
- * 
+ *
  * Call Blocking in Circuit Switched Networks
- * 
+ *
  * Copyright (C) 2014 Terence D. Todd
  * Hamilton, Ontario, CANADA
  * todd@mcmaster.ca
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 /*******************************************************************************/
@@ -38,36 +38,35 @@ void output_progress_msg_to_screen(Simulation_Run_Ptr this_simulation_run)
   double percentagedone;
   Simulation_Run_Data_Ptr sim_data;
 
-  sim_data = (Simulation_Run_Data_Ptr) simulation_run_data(this_simulation_run);
+  sim_data = (Simulation_Run_Data_Ptr)simulation_run_data(this_simulation_run);
 
   sim_data->blip_counter++;
 
-  if((sim_data->blip_counter >= BLIPRATE)
-     ||
-     (sim_data->number_of_calls_processed >= RUNLENGTH))
-    {
-      sim_data->blip_counter = 0;
+  if ((sim_data->blip_counter >= BLIPRATE) ||
+      (sim_data->number_of_calls_processed >= RUNLENGTH))
+  {
+    sim_data->blip_counter = 0;
 
-      percentagedone =
-	100 * (double) sim_data->number_of_calls_processed/RUNLENGTH;
+    percentagedone =
+        100 * (double)sim_data->number_of_calls_processed / RUNLENGTH;
 
-      printf("%3.0f%% ", percentagedone);
+    printf("%3.0f%% ", percentagedone);
 
-      printf("Call Count = %ld \r", sim_data->number_of_calls_processed);
+    printf("Call Count = %ld \r", sim_data->number_of_calls_processed);
 
-      fflush(stdout);
-    }
+    fflush(stdout);
+  }
 }
 
 /*******************************************************************************/
 
-void output_results(Simulation_Run_Ptr this_simulation_run)
+void output_results(Simulation_Run_Ptr this_simulation_run, FILE **writeFile)
 {
   double xmtted_fraction, wait_fraction;
   double average_wait_time, hang_up_probability;
   Simulation_Run_Data_Ptr sim_data;
 
-  sim_data = (Simulation_Run_Data_Ptr) simulation_run_data(this_simulation_run);
+  sim_data = (Simulation_Run_Data_Ptr)simulation_run_data(this_simulation_run);
 
   printf("\n");
 
@@ -75,25 +74,25 @@ void output_results(Simulation_Run_Ptr this_simulation_run)
   printf("call arrival count = %ld \n", sim_data->call_arrival_count);
   printf("calls that have to wait count = %ld \n", sim_data->wait_call_count);
   printf("hung up calls = %ld \n", sim_data->hang_up_call_count);
-  
-  //hang up probability
-  hang_up_probability = ((double) sim_data->hang_up_call_count)/(sim_data->call_arrival_count);
 
-  //actual value
-  average_wait_time = ( 60.0 * (double)sim_data->accumulated_wait_time)/(sim_data->call_arrival_count);
+  // hang up probability
+  hang_up_probability = ((double)sim_data->hang_up_call_count) / (sim_data->call_arrival_count);
 
-  xmtted_fraction = (double) (sim_data->call_arrival_count -
-      sim_data->wait_call_count)/sim_data->call_arrival_count;
+  // actual value
+  average_wait_time = (60.0 * (double)sim_data->accumulated_wait_time) / (sim_data->call_arrival_count);
 
-  printf("Blocking probability = %.5f (Service fraction = %.5f)\n",
-	 1-xmtted_fraction, xmtted_fraction);
+  xmtted_fraction = (double)(sim_data->call_arrival_count -
+                             sim_data->wait_call_count) /
+                    sim_data->call_arrival_count;
 
-  printf("Hang Up Probability = %.5f Average wait time= %.5f", hang_up_probability ,average_wait_time);
+  printf("Blocking probability = %.50f (Service fraction = %.5f)\n",
+         1 - xmtted_fraction, xmtted_fraction);
+
+  printf("Hang Up Probability = %.50f Average wait time= %.50f", hang_up_probability, average_wait_time);
 
   /* output order: w, N, Hang up Probability, average wait time */
 
   printf("\n");
+
+  fprintf(*writeFile, "%d, %.50f, %.50f, %d, %d \n", sim_data->random_seed, hang_up_probability, average_wait_time, OFFERED_LOAD, NUMBER_OF_CHANNELS);
 }
-
-
-
--- simparameters.h
+++ simparameters.h
@@ -1,26 +1,26 @@
 
 /*
- * 
+ *
  * Call Blocking in Circuit Switched Networks
- * 
+ *
  * Copyright (C) 2014 Terence D. Todd
  * Hamilton, Ontario, CANADA
  * todd@mcmaster.ca
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 /*******************************************************************************/
@@ -32,19 +32,17 @@
 
 #define Call_ARRIVALRATE 3   /* calls/minute */
 #define MEAN_CALL_DURATION 1 /* minutes */
-#define RUNLENGTH 5e6 /* number of successful calls */
+#define RUNLENGTH 5e6        /* number of successful calls */
 #define BLIPRATE 1e3
 #define NUMBER_OF_CHANNELS 1
-#define WAIT_THRESHOLD 3 /*minutes*/
+#define WAIT_THRESHOLD 3        /*minutes*/
 #define MEAN_ON_HOLD_DURATION 3 /*minutes*/
 
+#define OFFERED_LOAD (Call_ARRIVALRATE * MEAN_CALL_DURATION)
+
 /* Comma separated list of random seeds to run. */
-#define RANDOM_SEED_LIST 400191540, 400175089, 4001867733
+#define RANDOM_SEED_LIST 400191540, 400175089, 400186733
 
 /*******************************************************************************/
 
 #endif /* simparameters.h */
-
-
-
-
